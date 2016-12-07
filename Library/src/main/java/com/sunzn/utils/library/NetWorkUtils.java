0d2b69178ef5b4a60030cc8738adf8a47489ed29
ShortStickBoy <sunzn.cn@gmail.com>
2016-12-07 16:57:13 +0800

    Add NetWorkUtils

---
+++ Library/src/main/java/com/sunzn/utils/library/NetWorkUtils.java
@@ -0,0 +1,50 @@
+package com.sunzn.utils.library;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by sunzn on 2016/12/7.
+ */
+
+public class NetWorkUtils {
+
+    private NetWorkUtils() {
+        super();
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：获取活动网络信息
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 参数：context
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 权限：<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：NetworkInfo
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    private static NetworkInfo getActiveNetworkInfo(Context context) {
+        context = context.getApplicationContext();
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo();
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：判断网络是否连接
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 参数：context
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 权限：<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：{true ：是} {false ：否}
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    public static boolean isConnected(Context context) {
+        NetworkInfo info = getActiveNetworkInfo(context);
+        return info != null && info.isConnected();
+    }
+
+}

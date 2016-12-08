985b53c250a544d4578c7f64935ec6b83090ce27
ShortStickBoy <sunzn.cn@gmail.com>
2016-12-08 10:40:51 +0800

    Add CloseUtils

---
+++ Library/src/main/java/com/sunzn/utils/library/CloseUtils.java
@@ -0,0 +1,54 @@
+package com.sunzn.utils.library;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class CloseUtils {
+
+    private CloseUtils() {
+        super();
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：关闭 IO
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 参数：closeables 可关闭接口实现类
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：void
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    public static void closeIO(Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：安静关闭 IO
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 参数：closeables 可关闭接口实现类
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：void
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    public static void closeIOQuietly(Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+}

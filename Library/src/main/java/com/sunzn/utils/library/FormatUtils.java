c6ee40581bd61a8853221a381d6ee48ed041beea
sunzn <sunzn.cn@gmail.com>
2017-12-14 10:37:46 +0800

    Update FormatUtils

--- Library/src/main/java/com/sunzn/utils/library/FormatUtils.java
+++ Library/src/main/java/com/sunzn/utils/library/FormatUtils.java
@@ -1,6 +1,9 @@
 package com.sunzn.utils.library;
 
+import android.support.annotation.NonNull;
+
 import java.text.DecimalFormat;
+import java.util.Locale;
 
 /**
  * Created by sunzn on 2016/12/23.
@@ -24,6 +27,20 @@ public class FormatUtils {
     public static String format(float num) {
         DecimalFormat format = new DecimalFormat("0.00");
         return format.format(num);
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：格式化字符串
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 参数：format
+     * ║ 参数：args
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：格式化数字 String 值
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    public static String format(@NonNull String format, @NonNull Object... args) {
+        return String.format(Locale.getDefault(), format, args);
     }
 
 }

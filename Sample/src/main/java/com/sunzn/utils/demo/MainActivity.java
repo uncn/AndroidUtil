da045366f263575d29e30ae868d540cd273285a3
sunzn <sunzn.cn@gmail.com>
2018-10-25 14:47:20 +0800

    Update Sample

--- Sample/src/main/java/com/sunzn/utils/demo/MainActivity.java
+++ Sample/src/main/java/com/sunzn/utils/demo/MainActivity.java
@@ -1,9 +1,14 @@
 package com.sunzn.utils.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
-import com.sunzn.utils.library.ScreenUtils;
+import com.sunzn.utils.library.ColorUtils;
+import com.sunzn.utils.library.TimeUtils;
+
+import java.text.ParseException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ScreenUtils.getScreenWidth(this);
+        TextView zz = (TextView) findViewById(R.id.tv);
 
+        try {
+            long millis = TimeUtils.string2Millis("2017-08-22T16:42:35", "yyyy-MM-dd'T'HH:mm:ss");
+
+            String time = TimeUtils.millis2String(millis, "yyyy-MM-dd'T'HH:mm:ss");
+
+            zz.setText(time + " == " + millis);
+
+            zz.setText(ColorUtils.signColor("%s - 问题","关键词","#FF0000"));
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("sunzn", "MainActivity.onRestart()");
     }
 }
---
+++ Sample/src/main/java/com/sunzn/utils/demo/ThreeActivity.java
@@ -0,0 +1,28 @@
+package com.sunzn.utils.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by sunzn on 2017/7/5.
+ */
+
+public class ThreeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("MALL", "ThreeActivity.onDestroy()");
+    }
+
+}

c99e8f599d5b6b31902561051541b2df4305f978
sunzn <sunzn.cn@gmail.com>
2019-05-21 11:06:07 +0800

    Update DrawableUtils

--- Library/src/main/java/com/sunzn/utils/library/DrawableUtils.java
+++ Library/src/main/java/com/sunzn/utils/library/DrawableUtils.java
@@ -1,8 +1,13 @@
 package com.sunzn.utils.library;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.NinePatchDrawable;
 import android.os.Build;
@@ -25,6 +30,13 @@ public class DrawableUtils {
     private DrawableUtils() {
         throw new RuntimeException("Stub!");
     }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 变量：节省每次创建时产生的开销，但要注意多线程操作 synchronized
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    private static final Canvas sCanvas = new Canvas();
 
     /**
      * ╔════════════════════════════════════════════════════════════════════════════════════════════
@@ -422,6 +434,116 @@ public class DrawableUtils {
         final NinePatchDrawable drawable = (NinePatchDrawable) getDrawable(context, id);
         drawable.setColorFilter(new PorterDuffColorFilter(tintColor, PorterDuff.Mode.SRC_IN));
         return drawable;
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：从一个 view 创建原始大小的 Bitmap
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 参数：view   用于生成 Bitmap 的 view
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：Bitmap
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    public static Bitmap createBitmapFromView(@NonNull View view) {
+        return createBitmapFromView(view, 1f);
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：从一个 view 创建 Bitmap
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 参数：view    用于生成 Bitmap 的 view
+     * ║ 参数：scale   缩放比例，对创建的 Bitmap 进行缩放，数值支持从 0 到 1
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：Bitmap
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 注意：绘制之前要清掉 View 的焦点，因为焦点可能会改变一个 View 的 UI 状态。
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    public static Bitmap createBitmapFromView(@NonNull View view, float scale) {
+        if (view instanceof ImageView) {
+            Drawable drawable = ((ImageView) view).getDrawable();
+            if (drawable instanceof BitmapDrawable) {
+                return ((BitmapDrawable) drawable).getBitmap();
+            }
+        }
+        view.clearFocus();
+        Bitmap bitmap = createBitmapSafely((int) (view.getWidth() * scale), (int) (view.getHeight() * scale), Bitmap.Config.ARGB_8888, 1);
+        if (bitmap != null) {
+            synchronized (sCanvas) {
+                Canvas canvas = sCanvas;
+                canvas.setBitmap(bitmap);
+                canvas.save();
+                canvas.drawColor(Color.WHITE); // 防止 View 上面有些区域空白导致最终 Bitmap 上有些区域变黑
+                canvas.scale(scale, scale);
+                view.draw(canvas);
+                canvas.restore();
+                canvas.setBitmap(null);
+            }
+        }
+        return bitmap;
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：安全的从一个 view 创建 Bitmap
+     * ║ 描述：如果新建 Bitmap 时产生了 OOM，可以主动进行一次 GC - System.gc()，然后再次尝试创建
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 参数：width        Bitmap 宽度
+     * ║ 参数：height       Bitmap 高度
+     * ║ 参数：config       传入一个 Bitmap.Config
+     * ║ 参数：retryCount   创建 Bitmap 时产生 OOM 后，主动重试的次数
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：Bitmap
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    public static Bitmap createBitmapSafely(int width, int height, Bitmap.Config config, int retryCount) {
+        try {
+            return Bitmap.createBitmap(width, height, config);
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            if (retryCount > 0) {
+                System.gc();
+                return createBitmapSafely(width, height, config, retryCount - 1);
+            }
+            return null;
+        }
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：由一个 Drawable 生成 Bitmap
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 参数：view    用于生成 Bitmap 的 Drawable
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：Bitmap
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable == null)
+            return null;
+        else if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        int intrinsicWidth = drawable.getIntrinsicWidth();
+        int intrinsicHeight = drawable.getIntrinsicHeight();
+
+        if (!(intrinsicWidth > 0 && intrinsicHeight > 0))
+            return null;
+
+        try {
+            Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565;
+            Bitmap bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, config);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }

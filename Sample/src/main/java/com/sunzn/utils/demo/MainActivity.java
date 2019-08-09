48384fa07115fe945b06a2d913cded0e6cab98d4
sunzn <sunzn.cn@gmail.com>
2019-08-09 14:26:49 +0800

    Update Sample

--- Sample/src/main/java/com/sunzn/utils/demo/MainActivity.java
+++ Sample/src/main/java/com/sunzn/utils/demo/MainActivity.java
@@ -6,9 +6,12 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.sunzn.utils.library.ColorUtils;
+import com.sunzn.utils.library.StringUtils;
 import com.sunzn.utils.library.TimeUtils;
+import com.sunzn.utils.library.port.Connector;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView zz = (TextView) findViewById(R.id.tv);
+        TextView zz = findViewById(R.id.tv);
+
+        Log.e("TimeUtils", "Year = " + TimeUtils.getStartOfYear());
+        Log.e("TimeUtils", "Year = " + TimeUtils.millis2String(TimeUtils.getStartOfYear(), "yyyy-MM-dd HH:mm:ss zzzz"));
+
+        Log.e("TimeUtils", "=======================================");
+
+        Log.e("TimeUtils", "Year = " + TimeUtils.getEndOfYear());
+        Log.e("TimeUtils", "Year = " + TimeUtils.millis2String(TimeUtils.getEndOfYear(), "yyyy-MM-dd HH:mm:ss zzzz"));
 
         try {
             long millis = TimeUtils.string2Millis("2017-08-22T16:42:35", "yyyy-MM-dd'T'HH:mm:ss");
@@ -26,11 +37,20 @@ public class MainActivity extends AppCompatActivity {
 
             zz.setText(time + " == " + millis);
 
-            zz.setText(ColorUtils.signColor("%s - 问题","关键词","#FF0000"));
+            zz.setText(ColorUtils.signColor("%s - 问题", "关键词", "#FF0000"));
 
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
+        ArrayList<String> data = new ArrayList<>();
+
+        StringUtils.connectString(data, ";", new Connector<String>() {
+            @Override
+            public String getString(String s) {
+                return null;
+            }
+        });
 
     }
 

f3f13f8e6ed4c0ce4359a862f119e0a6f94bf73c
sunzn <sunzn.cn@gmail.com>
2020-02-24 15:34:51 +0800

    Update SDCardUtils

--- Library/src/main/java/com/sunzn/utils/library/SDCardUtils.java
+++ Library/src/main/java/com/sunzn/utils/library/SDCardUtils.java
@@ -1,6 +1,14 @@
 package com.sunzn.utils.library;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Environment;
+import android.os.StatFs;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 /**
  * Created by sunzn on 2016/12/7.
@@ -21,6 +29,58 @@ public class SDCardUtils {
      */
     public static boolean isSDCardEnable() {
         return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：获取SD卡路径
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：String
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    public static String getSDCardPath() {
+        if (!isSDCardEnable()) return "sdcard unable!";
+        String cmd = "cat /proc/mounts";
+        Runtime run = Runtime.getRuntime();
+        BufferedReader bufferedReader = null;
+        try {
+            Process p = run.exec(cmd);
+            bufferedReader = new BufferedReader(new InputStreamReader(new BufferedInputStream(p.getInputStream())));
+            String lineStr;
+            while ((lineStr = bufferedReader.readLine()) != null) {
+                if (lineStr.contains("sdcard") && lineStr.contains(".android_secure")) {
+                    String[] strArray = lineStr.split(" ");
+                    if (strArray.length >= 5) {
+                        return strArray[1].replace("/.android_secure", "") + File.separator;
+                    }
+                }
+                if (p.waitFor() != 0 && p.exitValue() == 1) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            CloseUtils.closeIO(bufferedReader);
+        }
+        return Environment.getExternalStorageDirectory().getPath() + File.separator;
+    }
+
+    /**
+     * ╔════════════════════════════════════════════════════════════════════════════════════════════
+     * ║ 名称：获取SD卡剩余空间
+     * ╟────────────────────────────────────────────────────────────────────────────────────────────
+     * ║ 返回：String
+     * ╚════════════════════════════════════════════════════════════════════════════════════════════
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static String getFreeSpace() {
+        if (!isSDCardEnable()) return "sdcard unable!";
+        StatFs stat = new StatFs(getSDCardPath());
+        long blockSize, availableBlocks;
+        availableBlocks = stat.getAvailableBlocksLong();
+        blockSize = stat.getBlockSizeLong();
+        return ConvertUtils.byte2FitMemorySize(availableBlocks * blockSize);
     }
 
 }
